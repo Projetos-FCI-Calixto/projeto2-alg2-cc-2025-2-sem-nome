3c5cd0da2bd5049748e3ac13b93d5a15b3f6dd1d
henrimartins-pogo <henri.martins2006@gmail.com>
2025-11-09 03:08:46 +0000

    atualizacao

--- projeto2.c
+++ projeto2.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+int codificacao(int v[], ){
+
+
+
+    return 0;
+}
+
+
+int corte(int v[], int n){
+
+    int meio = n/2;
+    int n1 = meio/2;
+    int n2 = meio;
+    int n3 = meio * 1.5;
+    int n4 = n;
+    return;
+}
+
 int main(){
+
+    int a, b;
+    int vetor [a][b];
+
+    codificacao();
     
+
     return 0;
 }
